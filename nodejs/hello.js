c079872ba0681d65284e246c060dc3698c250cbe
alexisVivier <alexis.vivier@ynov.com>
2019-05-27 10:53:39 +0200

    express install

---
+++ nodejs/hello.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env nodejs
+var http = require('http');
+http.createServer(function (req, res) {
+	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  res.end('Hello World\n');
+}).listen(1234, '172.17.111.26');
+console.log('Server running at http://localhost:8080/');
+
