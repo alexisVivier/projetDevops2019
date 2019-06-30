b7050035d1ebd5fd01efad6e5c4e44c5f4ecfd7a
ALEXIS <alexis@qlara.fr>
2019-06-30 15:20:50 +0200

    cfgvbhj

--- Docker/scripts/script.py
+++ Docker/scripts/script.py
@@ -10,45 +10,43 @@ i = 1
 with open('conf.json', 'r') as f:
     confstore = json.load(f)
 
-finalData = {"unitNumber" : confstore["unitNumber"],}
+finalData = {"unitNumber": confstore["unitNumber"], }
 
 # oldFileName = "1_" + str(datetime.datetime.now() - datetime.timedelta(minutes=1))
 
 while i <= 10:
-
     # if 'data.json':
     #     with open("%s.json" % oldFileName, 'r') as f:
     #         datastore = json.load(f)
 
     currAuto = "automata" + str(i)
 
-    #poids_lait_cuve_old = datastore[currAuto]["data"]["poids_lait_cuve"]
+    # poids_lait_cuve_old = datastore[currAuto]["data"]["poids_lait_cuve"]
     poids_lait_cuve = round(random.uniform(3512, 4607))
 
     data = {
-        "temp_cuve" : round(random.uniform(2.5, 4), 1),
-        "temp_ext" : round(random.uniform(8, 14), 1),
-        "poids_lait_cuve" : poids_lait_cuve,
-        #"poids_produit" : abs(poids_lait_cuve_old - poids_lait_cuve),
-        "poids_produit" : 0,
-        "mesure_ph" : round(random.uniform(6.8, 7.2), 1),
-        "mesure_k" : round(random.uniform(35, 47)),
-        "NaCi" : round(random.uniform(1, 1.7), 1),
-        "n_bact_salmon" : round(random.uniform(17, 37)),
-        "n_bact_ecoli" : round(random.uniform(35, 49)),
-        "n_bact_listeria" : round(random.uniform(28, 54)),
+        "temp_cuve": round(random.uniform(2.5, 4), 1),
+        "temp_ext": round(random.uniform(8, 14), 1),
+        "poids_lait_cuve": poids_lait_cuve,
+        # "poids_produit" : abs(poids_lait_cuve_old - poids_lait_cuve),
+        "poids_produit": 0,
+        "mesure_ph": round(random.uniform(6.8, 7.2), 1),
+        "mesure_k": round(random.uniform(35, 47)),
+        "NaCi": round(random.uniform(1, 1.7), 1),
+        "n_bact_salmon": round(random.uniform(17, 37)),
+        "n_bact_ecoli": round(random.uniform(35, 49)),
+        "n_bact_listeria": round(random.uniform(28, 54)),
     }
-    
-    finalData[currAuto] = {
-            "automataNumber" : confstore["automata"][currAuto]["automataNumber"],
-            "automataType" : confstore["automata"][currAuto]["automataType"],
-            "data" : data
-        },
 
-    i+=1
+    finalData[currAuto] = {
+                              "automataNumber": confstore["automata"][currAuto]["automataNumber"],
+                              "automataType": confstore["automata"][currAuto]["automataType"],
+                              "data": data
+                          },
+
+    i += 1
 
 fileName = "1_" + str(time.time()) + ".json"
-
 
 hote = "172.28.1.3"
 port = 1111
