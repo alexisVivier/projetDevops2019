994478346214f609e99201a0056c753fff566174
ALEXIS <alexis@qlara.fr>
2019-06-30 18:28:48 +0200

    fdghbj

--- Docker/concentrator/server.py
+++ Docker/concentrator/server.py
@@ -25,36 +25,35 @@ class ClientThread(threading.Thread):
 
         file = json.loads(r.decode('utf-8'))
 
-        for automates in file:
-            for automate in automates:
-                print("cc")
-                cnx = mysql.connector.connect(user='root', password='MariaDBroot2019', host='192.168.180.147', database='devops')
-                cursor = cnx.cursor()
+        for automate in file:
+            print(automate)
+            cnx = mysql.connector.connect(user='root', password='MariaDBroot2019', host='192.168.180.147', database='devops')
+            cursor = cnx.cursor()
 
-                insert = ("INSERT INTO automate "
-                          "(automate_number, automate_type, temp_cuve, temp_ext, poids_lait_cuve, poids_produit, mesure_ph, mesure_k, NaCi, n_bact_salmon, n_bact_listeria)"
-                          "VALUES (%(NUMBER)s, %(TYPE)s, %(TEMP_CUVE)s, %(TEMP_EXT)s, %(PLC)s, , %(PP)s, %(MPH)s, %(MK)s, %(NaCi)s, %(NBSALMON)s, %(NBECOLI)s, %(NBLISTERIA)s)"
-                          )
+            insert = ("INSERT INTO automate "
+                      "(automate_number, automate_type, temp_cuve, temp_ext, poids_lait_cuve, poids_produit, mesure_ph, mesure_k, NaCi, n_bact_salmon, n_bact_listeria)"
+                      "VALUES (%(NUMBER)s, %(TYPE)s, %(TEMP_CUVE)s, %(TEMP_EXT)s, %(PLC)s, , %(PP)s, %(MPH)s, %(MK)s, %(NaCi)s, %(NBSALMON)s, %(NBECOLI)s, %(NBLISTERIA)s)"
+                      )
 
-                data = {
-                    'NUMBER': automate['automataNumber'],
-                    'TYPE': automate['automataType'],
-                    'TEMP_CUVE': automate['data']['temp_cuve'],
-                    'TEMP_EXT': automate['data']['temp_ext'],
-                    'PLC': automate['data']['poids_lait_cuve'],
-                    'PP': automate['data']['poids_produit'],
-                    'MPH': automate['data']['mesure_ph'],
-                    'MK': automate['data']['mesure_k'],
-                    'NaCi': automate['data']['NaCi'],
-                    'NBSALMON': automate['data']['n_bact_salmon'],
-                    'NBECOLI': automate['data']['n_bact_ecoli'],
-                    'NBLISTERIA': automate['data']['n_bact_listeria'],
-                }
+            data = {
+                'NUMBER': automate['automataNumber'],
+                'TYPE': automate['automataType'],
+                'TEMP_CUVE': automate['data']['temp_cuve'],
+                'TEMP_EXT': automate['data']['temp_ext'],
+                'PLC': automate['data']['poids_lait_cuve'],
+                'PP': automate['data']['poids_produit'],
+                'MPH': automate['data']['mesure_ph'],
+                'MK': automate['data']['mesure_k'],
+                'NaCi': automate['data']['NaCi'],
+                'NBSALMON': automate['data']['n_bact_salmon'],
+                'NBECOLI': automate['data']['n_bact_ecoli'],
+                'NBLISTERIA': automate['data']['n_bact_listeria'],
+            }
 
-                cursor.execute(insert, data)
-                cnx.commit()
-                print("Donnée ajoutée")
-                cnx.close()
+            cursor.execute(insert, data)
+            cnx.commit()
+            print("Donnée ajoutée")
+            cnx.close()
 
 
         print("Client déconnecté...")
