11688e221a0b2651fd8ed030e17ef4d7f242defa
ALEXIS <alexis@qlara.fr>
2019-06-30 19:25:30 +0200

    xrdcftvg

--- Docker/scripts/script.py
+++ Docker/scripts/script.py
@@ -26,23 +26,21 @@ for i in range(1, 10):
     # poids_lait_cuve = round(random.uniform(3512, 4607))
 
     finalData[i] = {
-                       "unitNumber": confstore["unitNumber"],
-                       "automataNumber": confstore["automata"][currAuto]["automataNumber"],
-                       "automataType": confstore["automata"][currAuto]["automataType"],
-                       "data": {
-                           "temp_cuve": round(random.uniform(2.5, 4), 1),
-                           "temp_ext": round(random.uniform(8, 14), 1),
-                           "poids_lait_cuve": round(random.uniform(3512, 4607)),
-                           # "poids_produit" : abs(poids_lait_cuve_old - poids_lait_cuve),
-                           "poids_produit": 0,
-                           "mesure_ph": round(random.uniform(6.8, 7.2), 1),
-                           "mesure_k": round(random.uniform(35, 47)),
-                           "NaCi": round(random.uniform(1, 1.7), 1),
-                           "n_bact_salmon": round(random.uniform(17, 37)),
-                           "n_bact_ecoli": round(random.uniform(35, 49)),
-                           # "n_bact_listeria": round(random.uniform(28, 54)),
-                       }
-                   },
+        "unitNumber": confstore["unitNumber"],
+        "automataNumber": confstore["automata"][currAuto]["automataNumber"],
+        "automataType": confstore["automata"][currAuto]["automataType"],
+        "temp_cuve": round(random.uniform(2.5, 4), 1),
+        "temp_ext": round(random.uniform(8, 14), 1),
+        "poids_lait_cuve": round(random.uniform(3512, 4607)),
+        # "poids_produit" : abs(poids_lait_cuve_old - poids_lait_cuve),
+        "poids_produit": 0,
+        "mesure_ph": round(random.uniform(6.8, 7.2), 1),
+        "mesure_k": round(random.uniform(35, 47)),
+        "NaCi": round(random.uniform(1, 1.7), 1),
+        "n_bact_salmon": round(random.uniform(17, 37)),
+        "n_bact_ecoli": round(random.uniform(35, 49)),
+        # "n_bact_listeria": round(random.uniform(28, 54)),
+    }
 
     i += 1
 
